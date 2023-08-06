c622da7d4fe698654591cbe0b00bbfdcaa6ff5b7
Peter Riemenschneider <azulsoftwarevlc@gmail.com>
2023-08-06 17:24:28 +0200

    initial commit

--- creExcelWkb.go
+++ creExcelWkb.go
@@ -1,31 +1,87 @@
+
 package main
+
 
 import (
     "fmt"
+    "os"
+    "log"
 
+    util "github.com/prr123/utility/utilLib"
     "github.com/xuri/excelize/v2"
 )
 
 func main() {
-    f := excelize.NewFile()
+
+    numarg := len(os.Args)
+    dbg := false
+    flags:=[]string{"dbg","excel"}
+
+    // default file
+    xlsFilnam := ""
+
+    useStr := "./createExcelWkb /excel=excelfile [/dbg]"
+    helpStr := "program that creates an Excel file\n"
+
+    if numarg > len(flags) +1 {
+        fmt.Println("too many arguments in cl!")
+        fmt.Println("usage: ./template [/flag1=] [/flag2]\n", useStr)
+        os.Exit(-1)
+    }
+
+    if numarg > 1 && os.Args[1] == "help" {
+        fmt.Printf("help: %s\n", helpStr)
+        fmt.Printf("usage is: %s\n", useStr)
+        os.Exit(1)
+    }
+
+    flagMap, err := util.ParseFlags(os.Args, flags)
+    if err != nil {log.Fatalf("util.ParseFlags: %v\n", err)}
+
+    _, ok := flagMap["dbg"]
+    if ok {dbg = true}
+    if dbg {
+        fmt.Printf("dbg -- flag list:\n")
+        for k, v :=range flagMap {
+            fmt.Printf("  flag: /%s value: %s\n", k, v)
+        }
+    }
+
+    xlsval, ok := flagMap["excel"]
+    if !ok {
+        log.Fatalf("error -- no excel flag provided!")
+    } else {
+        if xlsval.(string) == "none" {log.Fatalf("error: no excel file provided with /excel flag!")}
+        xlsFilnam = xlsval.(string)
+//        if dbg {log.Printf("excel file: %s\n", xlsFilnam)}
+    }
+
+    log.Printf("debug: %t\n", dbg)
+	log.Printf("Using excel file: %s\n", xlsFilnam)
+
+
+
+    excelfil := excelize.NewFile()
     defer func() {
-        if err := f.Close(); err != nil {
-            fmt.Println(err)
+        if err := excelfil.Close(); err != nil {
+            log.Fatalf("error -- closing excel file: %v\n",err)
         }
     }()
+
     // Create a new sheet.
-    index, err := f.NewSheet("Sheet2")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+    index, err := excelfil.NewSheet("Sheet2")
+    if err != nil {log.Fatalf("error -- creating a Sheet: %v", err)}
+	log.Printf("index: %d\n",index)
+
     // Set value of a cell.
-    f.SetCellValue("Sheet2", "A2", "Hello world.")
-    f.SetCellValue("Sheet1", "B2", 100)
+    excelfil.SetCellValue("Sheet2", "A1", "Hello Sheet1: " + xlsFilnam)
+    excelfil.SetCellValue("Sheet1", "B1", "number")
+    excelfil.SetCellValue("Sheet1", "B2", 100)
     // Set active sheet of the workbook.
-    f.SetActiveSheet(index)
+    excelfil.SetActiveSheet(index)
     // Save spreadsheet by the given path.
-    if err := f.SaveAs("Book1.xlsx"); err != nil {
-        fmt.Println(err)
+    if err := excelfil.SaveAs(xlsFilnam); err != nil {
+        log.Fatalf("error -- saving excel file: %v\n", err)
     }
+
 }
